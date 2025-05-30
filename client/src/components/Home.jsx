56bb1fa81946216ef3a7ae0e0b7bceee96d807d4
Karl-Michael Partin <km.partin@gmail.com>
2025-05-30 15:41:45 +0200

    Home finished

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,37 +1,92 @@
 import FloatingShape from "./FloatingShape";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div className="min-h-[800px] h-full bg-gradient-to-br from-cyan-900 via-cyan-800 to-cyan-900 flex items-center justify-center relative overflow-hidden">
-      <FloatingShape
-        color="bg-cyan-700"
-        size="w-64 h-64"
-        top="0%"
-        left="20%"
-        delay={0}
-      />
-      <FloatingShape
-        color="bg-cyan-700"
-        size="w-48 h-48"
-        top="70%"
-        left="80%"
-        delay={5}
-      />
-      <FloatingShape
-        color="bg-cyan-700"
-        size="w-32 h-32"
-        top="40%"
-        left="10%"
-        delay={2}
-      />
+    <>
+      <div className="min-h-[800px] h-full bg-gradient-to-br from-cyan-900 via-cyan-800 to-cyan-900 flex flex-col relative overflow-hidden">
+        <FloatingShape
+          color="bg-cyan-700"
+          size="w-64 h-64"
+          top="0%"
+          left="20%"
+          delay={0}
+        />
+        <FloatingShape
+          color="bg-cyan-700"
+          size="w-48 h-48"
+          top="70%"
+          left="80%"
+          delay={5}
+        />
+        <FloatingShape
+          color="bg-cyan-700"
+          size="w-32 h-32"
+          top="40%"
+          left="10%"
+          delay={2}
+        />
 
-      <div>
-        <h1>Welcome to the openfunds universe!</h1>
-        <p className="text-center text-white">
-          Explore the openfunds field library .
-        </p>
+        <div className="ml-10 text-white">
+          <h1 className="text-4xl text-white font-bold mt-10 mb-10">
+            Manage the openfunds library
+          </h1>
+          <h2 className="text-3xl text-white font-bold mb-5">
+            Choose "Library" or "Tools" to get started.
+          </h2>
+
+          <h3 className="text-xl text-white font-bold mt-5 mb-2">
+            {" "}
+            <Link
+              to="/list"
+              className=" text-gray-100 hover:text-cyan-900 font-bold  border-2 border-cyan-900 hover:border-gray-100 bg-cyan-900 hover:bg-gray-100 rounded-lg  transition duration-300"
+            >
+              Library
+            </Link>
+          </h3>
+
+          <p>
+            With "Library", you can explore the openfunds fields shown as a list
+            of all fieds. <br />
+            For each field you can enter the details by clicking on the "Show"
+            button.{" "}
+          </p>
+
+          <h3 className="text-xl text-white font-bold mt-5 mb-2">
+            {" "}
+            <Link
+              to="/tools"
+              className=" text-gray-100 hover:text-cyan-900 font-bold  border-2 border-cyan-900 hover:border-gray-100 bg-cyan-900 hover:bg-gray-100 rounded-lg  transition duration-300"
+            >
+              Tools
+            </Link>
+          </h3>
+
+          <p>
+            With "Tools", you get access to exporting and importing features.
+          </p>
+
+          <h3 className="text-xl text-white font-bold mt-5 mb-2">
+            {" "}
+            <Link
+              to="/auth"
+              className=" text-gray-100 hover:text-cyan-900 font-bold  border-2 border-cyan-900 hover:border-gray-100 bg-cyan-900 hover:bg-gray-100 rounded-lg  transition duration-300"
+            >
+              Logging in
+            </Link>
+          </h3>
+
+          <p>
+            As a logged in user you can create new fields or edit and delete
+            existing fields.
+            <br />
+            User permissions are managed by the openfunds team. Only a few
+            administrators of the openfunds library will be granted write
+            permissions.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
